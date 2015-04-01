d360de4ab86ef08c64d9ae7e276558f2dec73c20
Tricia Bogen <tricia@cernu.us>
2015-03-31 22:16:25 -0700

    No linebreaks?

--- update.lua
+++ update.lua
@@ -3,20 +3,7 @@ opts = getopt( args, "" );
 
 base_url = "https://raw.githubusercontent.com/pdbogen/turtlesoft/master/";
 
-files = {
-	"/d"        = "dome.lua",
-	"/f"        = "fill.lua",
-	"/hypstruc" = "hyperboloid-structure.lua",
-	"/m"        = "mine.lua",
-	"/recroom"  = "rectangular-room.lua",
-	"/r"        = "roof.lua",
-	"/sc"       = "staircase.lua",
-	"/stdlib"   = "stdlib.lua",
-	"/t"        = "tunnel.lua",
-	"/unload"   = "unload.lua",
-	"/update"   = "update.lua",
-	"/w"        = "wall.lua",
-}
+files = { "/d" = "dome.lua", "/f" = "fill.lua", "/hypstruc" = "hyperboloid-structure.lua", "/m" = "mine.lua", "/recroom" = "rectangular-room.lua", "/r" = "roof.lua", "/sc" = "staircase.lua", "/stdlib" = "stdlib.lua", "/t" = "tunnel.lua", "/unload" = "unload.lua", "/update" = "update.lua", "/w" = "wall.lua", }
 
 for f,u in ipairs( files ) do
 	if( not ( opts[ "soft" ] and fs.exists( f ) ) ) then
