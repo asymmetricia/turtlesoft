d49d3ef4f59b61b15cf639678ff335157ec114b0
Tricia Bogen <tricia@cernu.us>
2015-03-31 22:34:29 -0700

    Retrieve and execute stdlib

--- update.lua
+++ update.lua
@@ -1,3 +1,14 @@
+if( not fs.exists( "/stdlib" ) ) then
+	local response = http.get( "http://www.pastebin.com/raw.php?i=e974cdJP" );
+	if response then
+		local sResponse = response.readAll(); response.close(); local file = fs.open( "/stdlib", "w" ); file.write( sResponse ); file.close();
+	else
+		print( "Error retrieving stdlib" );
+	end
+end
+
+dofile( "/stdlib" );
+
 args = {...}
 opts = getopt( args, "" );
 
