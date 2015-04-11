a9cd6329d62ba1d0cf40367d3b0fe56445e21c32
Tricia Bogen <tricia@cernu.us>
2015-04-11 15:18:33 -0700

    Add roof-every-nth for ceiling lighting

--- roof.lua
+++ roof.lua
@@ -6,20 +6,18 @@ else
 end
 
 args = {...}
-if( table.getn( args ) < 2 or table.getn( args ) > 3 ) then
-	print( "usage: roof <x> <y> [<match>]" );
+if( table.getn( args ) < 2 or table.getn( args ) > 4 ) then
+	print( "usage: roof <x> <y> [<n>] [<match>]" );
+	print( "	<n> -- only fill every n'th square" );
 	exit();
 end
 
-if( table.getn( args ) >= 3 ) then
-	match = 1;
-else
-	match = 0;
-end
+tx=0;    ty=0;   tz=0;
+modX=1;  modY=1; n=1;
+match=0;
 
-tx=0; ty=0; tz=0;
-modX=1; modY=1;
-
+if( table.getn( args ) >= 3 ) then n = tonumber(args[3]); end
+if( table.getn( args ) >= 4 ) then match = 1; end
 if( tonumber(args[1]) < 0 ) then modX = -1; end
 if( tonumber(args[2]) < 0 ) then modY = -1; end
 
@@ -34,19 +32,19 @@ while true do
 		end
 	end
 
-	if( x % 2 == 0 ) then
+	if( (x/n) % 2 == 0 ) then
 		-- even X
-		ty=ty+modY;
+		ty=ty+modY*n;
 		if( ty*modY >= tonumber(args[2])*modY ) then
-			ty=ty-modY;
-			tx=tx+modX;
+			ty=ty-modY*n;
+			tx=tx+modX*n;
 		end
 	else
 		-- odd X
-		ty=ty-modY;
+		ty=ty-modY*n;
 		if( ty*modY < 0 ) then
-			ty=ty+modY;
-			tx=tx+modX;
+			ty=ty+modY*n;
+			tx=tx+modX*n;
 		end
 	end
 	if( tx*modX > tonumber(args[1])*modX ) then
