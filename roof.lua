4acac5f40243e73b5c8a21e60b4072d358cbd434
Tricia Bogen <tricia@cernu.us>
2015-04-11 14:45:31 -0700

    Refactor with explicit matching, add ability to attack mobs when trying to place blocks

--- roof.lua
+++ roof.lua
@@ -6,9 +6,15 @@ else
 end
 
 args = {...}
-if( table.getn( args ) ~= 2 ) then
-	print( "usage: roof <x> <y>" );
+if( table.getn( args ) < 2 or table.getn( args ) > 3 ) then
+	print( "usage: roof <x> <y> [<match>]" );
 	exit();
+end
+
+if( table.getn( args ) >= 3 ) then
+	match = 1;
+else
+	match = 0;
 end
 
 tx=0; ty=0; tz=0;
@@ -18,6 +24,16 @@ if( tonumber(args[1]) < 0 ) then modX = -1; end
 if( tonumber(args[2]) < 0 ) then modY = -1; end
 
 while true do
+	find(1);
+	while match and not turtle.compareUp() do
+		turtle.digUp();
+	end
+	if not turtle.detectUp() then
+		while not turtle.placeUp() do
+			turtle.attackUp();
+		end
+	end
+
 	if( x % 2 == 0 ) then
 		-- even X
 		ty=ty+modY;
@@ -33,22 +49,9 @@ while true do
 			tx=tx+modX;
 		end
 	end
-	print( x .. "," .. y .. " => " .. tx .. "," .. ty );
-	if( tx*modX >= tonumber(args[1])*modX ) then
+	if( tx*modX > tonumber(args[1])*modX ) then
 		goto( 0, 0, 0 ); north();
-		find(1);
-		while not turtle.compareUp() do
-			turtle.digUp();
-			turtle.placeUp();
-			find(1);
-		end
 		break;
 	end
 	goto( tx, ty, tz );
-	find(1);
-	while not turtle.compareUp() do
-		turtle.digUp();
-		turtle.placeUp();
-		find(1);
-	end
 end
