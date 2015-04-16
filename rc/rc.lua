5add973fa74f46be9dde9903a81b5a7043d9798e
Tricia Bogen <tricia@cernu.us>
2015-04-16 10:58:47 -0700

    Add state display to UI

--- rc/rc.lua
+++ rc/rc.lua
@@ -13,22 +13,52 @@ function httpDownload( url, path )
         end
 end
 
-reactors = {}
+reactor = nil
 
-print( "Searching for reactors.." );
-for i,side in pairs(peripheral.getNames()) do
-	if( peripheral.getType( side ) == "BigReactors-Reactor" ) then
-		print( "Found reactor on " .. side .. " side" );
-		reactors[ side ] = peripheral.wrap( side );
+function findReactor()
+	for i,side in pairs(peripheral.getNames()) do
+		if( peripheral.getType( side ) == "BigReactors-Reactor" ) then
+			reactor = peripheral.wrap( side );
+		end
 	end
 end
 
+function printReactorState()
+	term.setCursorPos( 1, 2 );
+	if( sel == "state" ) then
+		print( "→" );
+	else
+		print( " " );
+	end
+
+	term.setCursorPos( 2, 2 );
+	print( "State: " );
+
+	term.setCursorPos( 9, 2 );
+	if( reactor.getActive() ) then
+		print( " Inactive  [Active]" );
+	else
+		print( "[Inactive]  Active" );
+	end
+end
+
+sel = 0;
+
 while 1 do
+	term.clear();
 	term.setCursorPos( 1, 1 );
 	print( "ReactorOS v0.1" );
-	for y = 2,19 do
-		term.setCursorPos( 1, y );
-		term.clearLine();
+	if( reactor ~= nil ) then
+		printReactorState();
+
+		for y = 3,19 do
+			term.setCursorPos( 1, y );
+			term.clearLine();
+		end
+	else
+		term.setCursorPos( 1, 2 );
+		print( "No Reactor Found" );
+		findReactor();
 	end
 	sleep(1);
 end
