8c73314070b5b337f178b1c8a9be3cd3008ea5cb
Tricia Bogen <tricia@cernu.us>
2015-12-26 10:51:11 -0800

    Match original semantics, where tunnel "begins" in block ahead of turtle; plus have turtle end at end of tunnel, not start.

--- tunnel.lua
+++ tunnel.lua
@@ -45,7 +45,8 @@ if opts["u"] ~= nil then
 	tx=0; ty=0; tz=0; dir=1;
 	-- Floor
 	while (tx < tunnel_x) do
-		while( (dir == 1 and ty < tunnel_y) or (dir == -1 and ty > -1) ) do
+		-- Note that tunnel runs form y=1 to y=(tunnel_y), i.e., starts one ahead of turtle starting pos
+		while( (dir == 1 and ty <= tunnel_y) or (dir == -1 and ty > 0) ) do
 			goto(tx,ty,tz);
 			placeBlockDown(1,match);
 			while(turtle.detectUp()) do turtle.digUp(); end
@@ -59,7 +60,7 @@ if opts["u"] ~= nil then
 	tx=tunnel_x; tz=1;
 	if dir == 1 then ty=0; else ty = tunnel_y-1; end
 	while (tz <= tunnel_z) do -- We'll go _above_ target Z
-		while( (dir == 1 and ty < tunnel_y) or (dir == -1 and ty > -1) ) do
+		while( (dir == 1 and ty <= tunnel_y) or (dir == -1 and ty > 0) ) do
 			goto(tx,ty,z); goto(x,y,tz);
 			placeBlockDown(1,match);
 			ty = ty + dir;
@@ -73,7 +74,7 @@ if opts["u"] ~= nil then
 	if dir == 1 then ty=0; else ty=tunnel_y-1; end
 	goto(x,y,tz);
 	while(tx > -1) do
-		while((dir == 1 and ty < tunnel_y) or (dir == -1 and ty > -1)) do
+		while((dir == 1 and ty <= tunnel_y) or (dir == -1 and ty > 0)) do
 			goto(tx,ty,tz);
 			placeBlockDown(1,match);
 			ty = ty + dir;
@@ -86,7 +87,7 @@ if opts["u"] ~= nil then
 	tx = -1; tz = 1;
 	if dir == 1 then ty=0; else ty=tunnel_y-1; end
 	while (tz <= tunnel_z) do -- We'll go _above_ target Z
-		while( (dir == 1 and ty < tunnel_y) or (dir == -1 and ty > -1) ) do
+		while( (dir == 1 and ty <= tunnel_y) or (dir == -1 and ty > 0) ) do
 			goto(tx,ty,z); goto(x,y,tz);
 			placeBlockDown(1,match);
 			ty = ty + dir;
@@ -95,7 +96,7 @@ if opts["u"] ~= nil then
 		dir = -1 * dir
 		tz = tz + 1
 	end
-	goto(x,y,z+1); goto(0,0,z); goto(x,y,tunnel_z-1); placeBlockUp(1,match); goto(0,0,0); north();
+	goto(x,y,z+1); goto(0,y,z); goto(0,tunnel_y,z); goto(x,y,tunnel_z-1); placeBlockUp(1,match); goto(x,y,0); north();
 else
 	tunnel( tunnel_x, tunnel_y, tunnel_z, 1, slope, match );
 end
