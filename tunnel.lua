2142781a0a962527920520218bed958563469185
Tricia Bogen <tricia@cernu.us>
2015-12-26 21:14:50 -0800

    Strip extra rparen

--- tunnel.lua
+++ tunnel.lua
@@ -106,7 +106,7 @@ if opts["u"] ~= nil then
 		dir = -1 * dir;
 		level = level + 1;
 	end
-	goto(x,y,z+1); goto(0,y,z); goto(x,y,sloped_z(y,slope)+tunnel_z-1)); placeBlockUp(1,match);
+	goto(x,y,z+1); goto(0,y,z); goto(x,y,sloped_z(y,slope)+tunnel_z-1); placeBlockUp(1,match);
 	while (y<tunnel_y) do
 		goto(x,y,sloped_z(y,slope));
 		goto(x,y+1,z);
