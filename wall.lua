17e2f609ed400d20ed1528989d770c834de36222
Tricia Bogen <tricia@cernu.us>
2017-06-18 16:08:40 -0700

    _actually_ adapt to negative z

--- wall.lua
+++ wall.lua
@@ -1,54 +1,55 @@
 if( fs.exists( "/stdlib" ) ) then
-	dofile( "/stdlib" );
+  dofile( "/stdlib" );
 else
-	print( "dome: error: /stdlib missing" );
-	exit();
+  print( "dome: error: /stdlib missing" );
+  exit();
 end
 
 args = {...}
 if( table.getn( args ) < 3 or table.getn( args ) > 4 ) then
-	print( "usage: wall {left,right} <y> <z> [<match>]" );
-	exit();
+  print( "usage: wall {left,right} <y> <z> [<match>]" );
+  exit();
 end
 
 if( table.getn( args ) < 4 ) then
-	match = 0;
+  match = 0;
 else
-	match = 1;
+  match = 1;
 end
 
 tx=0; ty=0; tz=0;
 
-target_y = tonumber(args[2]);
+target_z = tonumber(args[3])
+delta_z  = (target_z > 0 and 1) or -1
 
 while true do
-	if( args[1] == "left" ) then
-		west();
-	else
-		east();
-	end
+  if( args[1] == "left" ) then
+    west();
+  else
+    east();
+  end
 
-	placeBlock( 1, match );
+  placeBlock( 1, match );
 
-	if( y % 2 == 0 ) then
-		-- even Y
-		tz=tz+1;
-		if( tz >= tonumber(args[3]) ) then
-			tz=tz-1;
-			ty=ty+((target_y>0 and 1) or -1);
-		end
-	else
-		-- odd Y
-		tz=tz-1;
-				if( tz < 0 ) then
-			tz=tz+1;
-			ty=ty+((target_y>0 and 1) or -1);
-		end
-	end
-	if( target_y > 0 and ty >= target_y or target_y < 0 and ty <= target_y) then
-		goto(0,0,0);
-		north();
-		break;
-	end
-	goto( tx, ty, tz );
+  if( y % 2 == 0 ) then
+    -- even Y
+    tz=tz+delta_z;
+    if( tz >= target_z*delta_z ) then
+      tz=tz-delta_z;
+      ty=ty+1;
+    end
+  else
+    -- odd Y
+    tz=tz-delta_z;
+    if( tz*delta_z < 0 ) then
+      tz=tz+delta_z
+      ty=ty+1
+    end
+  end
+  if( ty >= tonumber(args[2]) ) then
+    goto(0,0,0);
+    north();
+    break;
+  end
+  goto( tx, ty, tz );
 end
