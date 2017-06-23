69151476a28e4354279ae28626303b3ace3f59f6
Tricia Bogen <tricia@cernu.us>
2017-06-22 19:05:40 -0700

    when tunneling up/down, first block should be up or down

--- stdlib.lua
+++ stdlib.lua
@@ -276,6 +276,14 @@ function tunnel( xdim, ydim, zdim, advance, slope, match )
 	slope = tonumber(slope) or 0
 	if(type(match) == "boolean") then match=match else match=false; end
 
+  if (slope < 0) then
+    goto(nil,1,nil);
+    goto(nil,nil,-1);
+  elseif (slope > 0) then
+    goto(nil,nil,1);
+    goto(nil,1,nil);
+  end
+
 	-- Z will actually take these values, i.e., minZ=maxZ=0 would be one block high
 	if( zdim < 0 ) then
 		minZ = z+zdim+1; maxZ = z;
