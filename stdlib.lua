343ebf1f30e517c9ca06d82721be8e4e4ec50def
Tricia Bogen <tricia@cernu.us>
2018-01-08 02:51:12 +0000

    report if matching enabled

--- stdlib.lua
+++ stdlib.lua
@@ -322,7 +322,8 @@ function tunnel( xdim, ydim, zdim, advance, slope, match )
 	ydim = tonumber(ydim) or 1
 	zdim = tonumber(zdim) or 2
 	slope = tonumber(slope) or 0
-	if(type(match) == "boolean") then match=match else print("expected boolean for match but received" .. type(match)); match=false; end
+	if(type(match) == "boolean") then match=match else print("tunnel: expected boolean for match but received" .. type(match)); match=false; end
+	if(match) then print("tunnel: block matching enabled"); end
 
 	-- Z will actually take these values, i.e., minZ=maxZ=0 would be one block high
 	if( zdim < 0 ) then
