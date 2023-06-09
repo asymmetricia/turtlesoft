e95ccd4a8a83b4eafaf684a950f18cafd1e029df
Tricia Bogen <tricia@cernu.us>
2023-06-09 08:09:19 -0700

    adjust start z

--- birch.lua
+++ birch.lua
@@ -15,7 +15,7 @@ for x=0,trees-1 do
   dx = x*5;
   for y=0,trees-1 do
     dy = y*5+1;
-    goto(dx, dy, 1);
+    goto(dx, dy, 0);
     placeBlockDown(1,1); -- place a sapling
     for z=1,7 do
       goto(dx,dy,dz); -- dig out the tree
