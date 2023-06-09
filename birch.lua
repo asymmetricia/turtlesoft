a2fe1d3c0645eb95d9fa693317c7536c525789c5
Tricia Bogen <tricia@cernu.us>
2023-06-09 08:11:17 -0700

    correct iterator

--- birch.lua
+++ birch.lua
@@ -17,7 +17,7 @@ for x=0,trees-1 do
     dy = y*5+1;
     goto(dx, dy, 0);
     placeBlockDown(1,1); -- place a sapling
-    for z=1,7 do
+    for dz=1,7 do
       goto(dx,dy,dz); -- dig out the tree
     end
   end
