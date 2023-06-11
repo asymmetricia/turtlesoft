bddb5e9087a67572a288c6cfae273365fefd872a
Tricia Bogen <tricia@cernu.us>
2023-06-10 19:52:24 -0700

    optimized tree hunting

--- birch.lua
+++ birch.lua
@@ -11,14 +11,29 @@ end
 
 trees = args[1];
 
-for x=0,trees-1 do
-  dx = x*5;
-  for y=0,trees-1 do
-    dy = y*5+1;
-    goto(dx, dy, 0);
+max=trees-1
+
+for dx=0,max*5,10 do
+  for dy=1,max*5+1,10 do
+    goto(dx, dy, 0);     -- goto next tree
     placeBlockDown(1,1); -- place a sapling
-    for dz=1,7 do
-      goto(dx,dy,dz); -- dig out the tree
+    goto(dx, dy, 7);     -- mine out the tree
+    if( dy+5 <= max*5+1 ) then
+      goto(dx, dy+5, 7);   -- move to next tree
+      goto(dx, dy+5, 0);   -- mine out the tree
+      placeBlockDown(1,1); -- place a sapling
+    end
+  end
+  if (dx+5 <= max) then
+    for dy=max*5+1,1,-10 do
+      goto(dx, dy, 7);     -- goto next tree
+      goto(dx, dy, 0);     -- mine out the tree
+      placeBlockDown(1,1); -- place a sapling
+      if( dy-5 >= 1) then
+        goto(dx, dy-5, 0);   -- mine out the tree
+        placeBlockDown(1,1); -- place a sapling
+        goto(dx, dy-5, 7);   -- move to next tree
+      end
     end
   end
 end
