fd3c3a9ffa3889d3b1b1004eb3387ed5ce05cc07
Tricia Bogen <tricia-github@cernu.us>
2026-01-02 19:13:07 -0800

    row at a time plus lighting

--- expo.lua
+++ expo.lua
@@ -9,13 +9,30 @@ if( table.getn( args ) < 2 or table.getn( args ) > 2 ) then
   error( "usage: expo <x> <y>" );
 end
 
--- dig forward
-newMineArea( 1, tonumber(args[2]), 3, 0 );
-
 for ty=0,tonumber(args[2]),3 do
+  -- dig out sideways
+  newMineArea( tonumber(args[1]), 1, 3, 0 );
+
+  -- return to row origin
   goto(0,ty,0);
-  newMineArea( tonumber(args[1]), 16, 3, 0 );
+
+  -- dig out straight
+  newMineArea( 1, 3, 3, 0 );
+
+  -- place torches
+  for tx=0,tonumber(args[1]),7 do
+    goto(tx, ty, 1);
+    placeBlockDown(1, true);
+  end
+
+  -- return to row origin
+  goto(0,ty,1);
+
+  -- advanaced forward
+  goto(0,ty+3,1);
+
+  -- reset downward
+  goto(0,ty+3,0);
 end
 
-goto(0,0,0);
 north();
